eba32cb5e84a835527aae7c42a16e59db0016174
daenerystrueborn-lang <daenerystrueborn@gmail.com>
2026-03-26 05:48:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.options("*", (req, res) => {
 app.use(
   "/api",
   createProxyMiddleware({
-    target: "http://5.39.63.207:7777",
+    target: "http://5.39.63.207:6253",
     changeOrigin: true,
     proxyTimeout: 25000,   // 25s — under Vercel's 30s maxDuration
     timeout: 25000,
